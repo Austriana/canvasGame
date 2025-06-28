8a20582e5bbea4be20aef761f6dc3a96eeefc7eb
Austriana <Marco.Mang@gmx.de>
2025-06-28 07:47:04 +0000

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler{
+        constructor(){
+            
+        }
+    }
+})
