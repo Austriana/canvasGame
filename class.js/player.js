7a28d4b61b1f15d266a10815bd938ed00d24f804
Austriana <Marco.Mang@gmx.de>
2025-07-24 15:31:26 +0200

    .......

--- class.js/player.js
+++ class.js/player.js
@@ -8,7 +8,7 @@ export default class Player{
         this.gameHeigth = gameHeigth;
         this.width = 200;
         this.height = 200;
-        this.x = 0;
+        this.x = this.width;
         this.y = this.gameHeigth - this.height;
         this.frameX = 0;
         this.frameY = 0;
